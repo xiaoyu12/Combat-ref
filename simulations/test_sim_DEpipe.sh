cc3b86718601ff7d9f27b6de678a4633a9af65fa
xiaoyu12 <xiaoyu.zhang@gmail.com>
2025-08-03 13:07:44 -0700

    local changes

--- simulations/test_sim_DEpipe.sh
+++ simulations/test_sim_DEpipe.sh
@@ -1,18 +1,18 @@
-Rscript sim_DEpipe.R 1 1 12 &
-Rscript sim_DEpipe.R 1 2 12 &
-Rscript sim_DEpipe.R 1 3 12 &
-Rscript sim_DEpipe.R 1 4 12 &
-Rscript sim_DEpipe.R 1.5 1 12 &
-Rscript sim_DEpipe.R 1.5 2 12 &
+Rscript sim_DEpipe.R 1 1 200 &
+Rscript sim_DEpipe.R 1 2 200 &
+Rscript sim_DEpipe.R 1 3 200 &
+Rscript sim_DEpipe.R 1 4 200 &
+Rscript sim_DEpipe.R 1.5 1 200 & 
+Rscript sim_DEpipe.R 1.5 2 200 &
 
-Rscript sim_DEpipe.R 1.5 3 12 &
-Rscript sim_DEpipe.R 1.5 4 12 &
-Rscript sim_DEpipe.R 2 1 12 &
-Rscript sim_DEpipe.R 2 2 12 &
-Rscript sim_DEpipe.R 2 3 12 &
-Rscript sim_DEpipe.R 2 4 12 &
-Rscript sim_DEpipe.R 2.4 1 12 &
-Rscript sim_DEpipe.R 2.4 2 12 &
-Rscript sim_DEpipe.R 2.4 3 12 &
-Rscript sim_DEpipe.R 2.4 4 12 &
+Rscript sim_DEpipe.R 1.5 3 200 &
+Rscript sim_DEpipe.R 1.5 4 200 &
+Rscript sim_DEpipe.R 2 1 200 &
+Rscript sim_DEpipe.R 2 2 200 &
+Rscript sim_DEpipe.R 2 3 200 &
+Rscript sim_DEpipe.R 2 4 200 &
+Rscript sim_DEpipe.R 2.4 1 200 & 
+Rscript sim_DEpipe.R 2.4 2 200 &
+Rscript sim_DEpipe.R 2.4 3 200 &
+Rscript sim_DEpipe.R 2.4 4 200 &
 
